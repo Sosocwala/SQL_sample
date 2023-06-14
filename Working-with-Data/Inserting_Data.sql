ca706c8b3dc7d5cdc7df49fe9e6f48d3810ba4cb
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-06-14 20:46:45 +0200

    Inserting The Data

---
+++ Working-with-Data/Inserting_Data.sql
@@ -0,0 +1,4 @@
+Select * from Customer_table
+
+insert into Customer_table (First_Name, Last_Name, Street_Address, City, State_Province, Zip_Code, Phone_Number)
+Values('Mark', 'Van Heerden', '19 Falcon Avenue', 'Johannesburg', DEFAULT, '5320', '071 152 8111')
