50416615755079f7a31bd61f07c751d9dbd92aae
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-06-15 10:51:05 +0200

    Deleting Data

---
+++ Working-with-Data/Deleting_Data.sql
@@ -0,0 +1,10 @@
+--View all Customers
+Select * From Customer_table
+
+--Delete a single record - Specify a where clause
+Delete From Customer_table
+Where CustomerID = '2'
+
+--Delete All Records
+Delete from Customer_table
+
